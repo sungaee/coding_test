24a3bde38f7ed55aed5ab6a10630d201b3d8ed2b
sungaee <64218623+sungaee@users.noreply.github.com>
2026-07-27 14:31:36 +0900

    [level 0] Title: 수열과 구간 쿼리 2, Time: 2.94 ms, Memory: 50.7 MB -BaekjoonHub

---
+++ 프로그래머스/0/181923. 수열과 구간 쿼리 2/수열과 구간 쿼리 2.js
@@ -0,0 +1,8 @@
+function solution(arr, queries) {
+    return queries.map(([s,e,k]) => {
+        const filtered = arr.slice(s, e + 1).filter(val => val > k);
+        
+        return filtered.length > 0 ? Math.min(...filtered) : -1;
+    })
+
+}
