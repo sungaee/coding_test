38308f4eb3af1a9dde0e0be3a9d8c291b84e61d3
sungaee <64218623+sungaee@users.noreply.github.com>
2026-07-28 16:02:24 +0900

    [level 0] Title: 배열 만들기 4, Time: 3.33 ms, Memory: 51.3 MB -BaekjoonHub

---
+++ 프로그래머스/0/181918. 배열 만들기 4/배열 만들기 4.js
@@ -0,0 +1,17 @@
+function solution(arr) {
+    var stk = [];
+    let i = 0;
+    
+    while(i < arr.length){
+        if(stk.length === 0){
+            stk.push(arr[i])
+            i++
+        }else if(stk.at(-1) < arr[i]){
+            stk.push(arr[i])
+            i++
+        }else{
+            stk.pop()
+        }
+    }
+    return stk;
+}
