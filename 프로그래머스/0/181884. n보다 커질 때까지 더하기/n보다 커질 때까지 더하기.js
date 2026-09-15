625964093e80e2911a90e7405e323060174000b4
sungaee <64218623+sungaee@users.noreply.github.com>
2026-09-15 12:27:17 +0900

    [level 0] Title: n보다 커질 때까지 더하기, Time: 0.11 ms, Memory: 43.7 MB -BaekjoonHub

---
+++ 프로그래머스/0/181884. n보다 커질 때까지 더하기/n보다 커질 때까지 더하기.js
@@ -0,0 +1,10 @@
+function solution(numbers, n) {
+    var sum = 0;
+    for(let i = 0; i < numbers.length; i++){
+        sum += numbers[i]         
+        if(sum > n){
+            break
+        }
+    }
+    return sum
+}
